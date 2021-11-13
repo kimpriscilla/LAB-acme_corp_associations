1b986ab09440c9ecba370f3879f74d5c816d08bd
Priscilla <priscillakim58@gmail.com>
2021-11-13 01:21:07 -0500

    set routes

--- server.js
+++ server.js
@@ -6,11 +6,23 @@ const db = new Sequelize(
 const express = require("express");
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("/api/departments", async (req, res) => {
   try {
     res.send(
       await Department.findAll({
         include: [{ model: Employee, as: "manager" }], //! alias does 2 things, 1) sets the foreign key and 2) sets how it ends up getting loaded
+      })
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/employees", async (req, res) => {
+  try {
+    res.send(
+      await Employee.findAll({
+        include: [{ model: Employee, as: "supervisor" }, Employee, Department],
       })
     );
   } catch (error) {
@@ -38,17 +50,24 @@ const Employee = db.define("employee", {
 Department.belongsTo(Employee, { as: "manager" }); //!now employeeId is now under managerId
 Employee.hasMany(Department, { foreignKey: "managerId" }); //! use thru table to associate foreignKey so we dont have both managerId & employeeId, just managerId
 
+Employee.belongsTo(Employee, { as: "supervisor" });
+Employee.hasMany(Employee, { foreignKey: "supervisorId" });
+
 const syncAndSeed = async () => {
   await db.sync({ force: true });
-  const [moe, lucy, hr, engineering] = await Promise.all([
+  const [moe, lucy, larry, hr, engineering] = await Promise.all([
     Employee.create({ name: "moe" }),
     Employee.create({ name: "lucy" }),
+    Employee.create({ name: "larry" }),
     Department.create({ name: "hr" }),
     Department.create({ name: "engineering" }),
   ]);
   hr.managerId = lucy.id;
   await hr.save();
   //console.log(JSON.stringify(hr, null, 2));
+  moe.supervisorId = lucy.id;
+  larry.supervisorId = lucy.id;
+  await Promise.all([moe.save(), larry.save()]);
 };
 
 const start = async () => {
